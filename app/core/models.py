ff34a1a0932582b79fcf08913a2e195f5a4cf85c
Stefan Witchell <stefanwitchell1990@gmail.com>
2019-10-28 21:55:15 +0000

    Added recipe image upload feature.

--- app/core/models.py
+++ app/core/models.py
@@ -84,7 +84,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
-    imFW = models.ImageField(null=True, upload_to=recipe_image_file_path)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
